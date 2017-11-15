3b68de956de65791cfdf7363a0e1d19e97820ca2
Daniel Tolbert <danielptolbert@gmail.com>
2017-11-15 11:18:50 -0500

    small tweaks

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ module.exports = function ($) {
 		$('title').text(newTitle);
 	}
 
-	$('ul.summary a:contains(FAQ),ul.summary a:contains(GitHub)').append('&nbsp;<i class="icons8-open-in-window" style="float:right;"></i>');
+	$('ul.summary a:contains(FAQ),ul.summary a:contains(GitHub),ul.summary a:contains(Support)').append('&nbsp;<i class="icons8-open-in-window" style="float:right;"></i>');
 	$('a').has('button, span').css('border-bottom','0px')
 	$('.markdown-section').has('.api-method-code').css('padding-top','0px');
 	$('.page-inner').has('iframe').contents().find('div, section').css({'height':'100vh','padding':'0px'});
