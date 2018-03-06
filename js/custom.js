c02dd40da41e4746650b4c1d0fcfbf9540b82a47
Daniel Tolbert <dtolbert@bandwidth.com>
2018-03-06 11:28:07 -0500

    update to call screen

--- js/custom.js
+++ js/custom.js
@@ -13,5 +13,21 @@ module.exports = function ($) {
 	$('.page-inner').has('iframe').contents().find('div, section').css({'height':'100vh','padding':'0px'});
 	$('ul.summary a:contains(Full API Reference),ul.summary a:contains(BXML Reference),ul.summary a:contains(Messaging v2 Reference),ul.summary a:contains(Phone Numbers Reference) ').removeAttr('target');
 	$('.footerListItem a, a[href*="docs/phone-numbers"]').removeAttr('target');
+
+  var helperPages = [
+    'callScreen.html'
+  ];
+
+
+  $('li.chapter a').each(function(i, elem) {
+    var a = $(elem);
+    var link = a.attr("href");
+    helperPages.forEach(page => {
+      if(link.endsWith(page)) {
+        a.attr("href", link+'#top');
+      }
+    });
+  });
+
 	return $.html();
 }
