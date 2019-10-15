4b16bdfec7b60ca211822a87f0de93e352a06b83
Aubron Wood <aubron@daemon.group>
2019-10-15 12:56:53 -0400

    Add full deployment script

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,23 @@ function doCompile {
   cp -a _book/. out/
 }
 
-function installLambdaDependencies {
+function installAndBuildLambda {
   cd ./lambda
   yarn install
+  yarn build
+  cd ../
+}
+
+function testLambda {
+  cd ./lambda
+  yarn test
+  cd ../
+}
+
+function deployLambda {
+  cd ./lambda
+  cdk deploy
+  yarn deployLambdaEdge
   cd ../
 }
 
@@ -19,24 +33,25 @@ function installLambdaDependencies {
 doCompile
 
 # install lambda dependencies
-installLambdaDependencies
+installAndBuildLambda
+# Test lambda
+testLambda
 
 
 #If it is the target branch and NOT a pull request, then deploy 
 if [ "$TRAVIS_BRANCH" == "$TARGET_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
+  
   # deploy the site to s3
   aws s3 sync ./out/ s3://stop-gap --delete
   
-  
+  # deploy the lambda function
+  deployLambda
+
   # Clear the cloudfront cache
   aws cloudfront create-invalidation --distribution-id E2DJMH5LKMGQA6 --paths "/*"
   printf "%s\n" "master branch"
 else
-
-  # Run tests
-  cd ./lambda
-  yarn test
 
   # do nothing, and leave s3-deploy post script handle deployment.
   printf "%s\n" "another branch"
